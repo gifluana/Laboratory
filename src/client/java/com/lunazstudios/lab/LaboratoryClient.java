43d7eb2ca7fa50b4a73496a0626fed7d61027074
gifluana <gif.luana@outlook.com>
2024-03-28 09:47:26 -0300

    Initial commit

---
+++ src/client/java/com/lunazstudios/lab/LaboratoryClient.java
@@ -0,0 +1,10 @@
+package com.lunazstudios.lab;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LaboratoryClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
